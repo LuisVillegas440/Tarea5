a5a129430f145fd9a6835db4fe0e771df844daf7
Luis Villegas <lvillegasc2@unemi.edu.ec>
2024-07-01 14:38:21 -0500

    Ya esta todo

--- Javascript/ejercicio01.js
+++ Javascript/ejercicio01.js
@@ -1,8 +1,10 @@
 function par(){
     let numero = parseInt(document.querySelector("#numero1").value);
+    let numero2 = parseInt(document.querySelector("#numero2").value);
     document.querySelector("#numero1").value = "";
+    document.querySelector("#numero2").value = "";
     document.querySelector("#resultado").innerHTML = "";
-    for(let i = 1; i <= numero; i++){
+    for(let i = numero; i <= numero2; i++){
         if(i % 2 === 0){
         document.querySelector("#resultado").innerHTML += `${i}<br>`;
         }
